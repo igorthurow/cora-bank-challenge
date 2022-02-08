04a63dbd9c05239eb556c53a5248c1f445359cb5
Igor Thurow <igorthurow@gmail.com>
2022-02-08 16:29:24 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+      title: `Cora Bank Challenge`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"]
+};
