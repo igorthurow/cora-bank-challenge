52fc2c7ceb02205122e96d5a2f6d694e6133c8b0
Igor Thurow <igorthurow@gmail.com>
2022-02-08 21:52:59 -0300

    Change normalize.css for reset.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,44 @@
-import React from "react"
+import React from 'react'
 
 import './index.scss'
-import 'normalize.css'
+import 'reset-css'
 
 import { Helmet } from 'react-helmet'
 
-import { MenuItem } from '../components'
+import { MenuItem, Menu } from '../components'
 
 const IndexPage = () => {
   return (
 		<div>
 			<Helmet />
-			<MenuItem route={'igor'} >Tudo bem?</MenuItem>
+			<Menu
+				menuItems={[
+					{
+						label: 'Igor',
+						route: '/'
+					},
+					{
+						label: 'Igor',
+						route: '/'
+					},
+					{
+						label: 'Igor',
+						route: '/'
+					},
+					{
+						label: 'Igor',
+						route: '/'
+					},
+					{
+						label: 'Igor',
+						route: '/'
+					},
+					{
+						label: 'Igor',
+						route: '/'
+					},
+				]}
+			/>
 		</div>
   )
 }
